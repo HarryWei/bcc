b5a88749955348d3565a00e58349e090c9b7a901
Weiwei Jia <harryxiyou@gmail.com>
2017-09-07 10:42:47 -0400

    tune vCPU nums

--- tools/IOMigrater/backstage.c
+++ tools/IOMigrater/backstage.c
@@ -102,7 +102,7 @@ void init_cpu_thread(void) {
 	p = (pthread_t *) malloc(sizeof(pthread_t) * vcpu_num);
 	if (p == NULL) handle_error("malloc error!");
 
-	for (i = 0; i < vcpu_num; i++) {
+	for (i = 1; i < vcpu_num - 1; i++) {
 		//_vcpu_num[i] = i;
 		g_mutex_lock(&mutex);
 		printf("i is %lu\n", i);
