4ea6ab1bb885b7959896583f079f8ba34260fabe
Weiwei Jia <wj47@njit.edu>
2017-11-21 07:10:58 -0500

    test simple single I/O thread migration

--- vMigrater/tests/guest_part_read_unittest.py
+++ vMigrater/tests/guest_part_read_unittest.py
@@ -151,16 +151,32 @@ while 1:
         exiting = 1
 # timestamp is in microseconds.
     #vCPUs = []
+    prev=int(time.time() * pow(10,6))
     with open(vCPU_Sorted_RTS, 'r') as infile:
         try:
             vCPUs = json.load(infile)
         except ValueError:
             pass
+    _now=int(time.time() * pow(10,6))
+    print("load file cost is %d us" % (_now - prev))
+    prev=int(time.time() * pow(10,6))
+    os.sched_setaffinity(pid, {1})
+    _now=int(time.time() * pow(10,6))
+    print("set affinity cost is %d us" % (_now - prev))
+    prev=int(time.time() * pow(10,6))
+    try:
+        affinity = os.sched_getaffinity(pid)
+    except ProcessLookupError:
+        print("Task %d might be finished (or not I/O intensive)" % pid)
+    vcpu = affinity.pop()
+    _vcpu = int(vcpu)
+    _now=int(time.time() * pow(10,6))
+    print("get affinity cost is %d us" % (_now - prev))
     #print(vCPUs)
-    print("There are %d available vCPUs running!" % len(vCPUs))
-    for i in vCPUs:
+    #print("There are %d available vCPUs running!" % len(vCPUs))
+    #for i in vCPUs:
         #print("vCPU %d has remaining %d us" % (int(i[1]), int(i[0])))
-        print("vCPU %d has remaining %d us" % (i[1], i[0]))
+        #print("vCPU %d has remaining %d us" % (i[1], i[0]))
     if exiting:
         print("Exiting...")
         exit()
--- vMigrater/tests/migrate_single_io_thread.py
+++ vMigrater/tests/migrate_single_io_thread.py
@@ -27,170 +27,44 @@ import json
 
 
 # IOMigrater constants
-host_dir = "/sys/module/core/parameters/"
 vCPU_Sorted_RTS = "/mnt/sorted"
-vCPU_num = 9
-vCPU_start = 1
-vCPU_end = 9
-
-# for do_migration_v2
-time_flag = 0
-vcpu_flag = 0
-prev_us = 0
-now_us = 0
-
-def ReadFile(filepath):
-  f = open(filepath, "r")
-  try:
-    contents = f.read()
-  finally:
-    f.close()
-
-  return contents
-
-def WriteFile(filepath, buf):
-  f = open(filepath, "w")
-  try:
-    f.write(buf)
-  finally:
-    f.close()
-
-def migration_check(pid):
-    try:
-        affinity = os.sched_getaffinity(pid)
-    except ProcessLookupError:
-        print("Task %d might be finished (or not I/O intensive)" % pid)
-        return -1
-    vcpu = affinity.pop()
-    _vcpu = int(vcpu)
-    print('Task %d is running on CPU %d' % (pid, _vcpu))
-    is_vCPU_on_path = host_dir + "vm1_is_vcpu%d_on" % _vcpu
-    vCPU_curr_ts_path = host_dir + "vm1_vcpu%d_curr_ts" % _vcpu
-    vCPU_prev_timeslice_path = host_dir + "vm1_vcpu%d_ts" % _vcpu
-    if os.path.exists(is_vCPU_on_path) and os.path.exists(vCPU_curr_ts_path) and os.path.exists(vCPU_prev_timeslice_path):
-        is_vCPU_on = ReadFile(is_vCPU_on_path)
-        if int(is_vCPU_on) == 1:
-            vCPU_curr_ts = ReadFile(vCPU_curr_ts_path)
-            guest_curr_ts = time.time() * pow(10, 6)
-            vCPU_used_timeslice = int(guest_curr_ts) - int(vCPU_curr_ts)
-            vCPU_prev_timeslice = ReadFile(vCPU_prev_timeslice_path)
-            vCPU_remaining_timeslice = int(vCPU_prev_timeslice) - vCPU_used_timeslice
-            if vCPU_remaining_timeslice < 2000:
-                return 1
-            else:
-                return 0
-        else:
-            return 1		
-    else:
-        sys.exit("Error: Cannot find %s file." % (is_vCPU_on_path))
-        return -1
-
-def do_migration_v1(pid):
-    flag = migration_check(pid)
-    if flag == 1:
-        vCPUs = get_available_vCPUs()
-        if len(vCPUs) != 0:
-            vCPU = vCPUs[len(vCPUs) - 1][1]
-            vCPU_remaining_timeslice = vCPUs[len(vCPUs) - 1][0]
-            print("Biggest remaining timeslice vCPU is %d, and remaining timeslice is %d" % (vCPU, vCPU_remaining_timeslice))
-            if vCPU_remaining_timeslice >= 3000:
-                try:
-                    os.sched_setaffinity(pid, {vCPU})
-                    return 1
-                except OSError:
-                    print ("Catch OSError: process %d might not be migrated" % pid)
-                    return -1
-            else:
-                return 0
-        else:
-            return 0
-    else:
-        return 0
-
-def do_migration_v2(pid):
-    if vcpu_flag == 0:
-        vCPUs = get_available_vCPUs()
-        vcpu_flag = 1
-        if len(vCPUs) != 0:
-            vCPU = vCPUs[len(vCPUs) - 1][1]
-            vCPU_remaining_timeslice = vCPUs[len(vCPUs) - 1][0]
-            print("Biggest remaining timeslice vCPU is %d, and remaining timeslice is %d" % (vCPU, vCPU_remaining_timeslice))
-            try:
-                os.sched_setaffinity(pid, {vCPU})
-                return 1
-            except OSError:
-                print ("Catch OSError: process %d might not be migrated" % pid)
-                return -1
-    if time_flag == 0:
-        prev = time.monotonic()
-        prev_us = int(now * pow(10, 6))
-        time_flag = 1
-    else:
-        now = time.monotonic()
-        now_us = int(now * pow(10, 6))
-        diff_us = now_us - prev_us
-        prev_us = now_us
-    if (vCPU_remaining_timeslice - diff_us) <= 2000:
-        vcpu_flag = 1
-
-    return 0
 				
-
-#print('Tracing... Output every %d secs. Hit Ctrl-C to end' % interval)
-print('vMigrater: VMM Kernel part unittest...')
-#Set VMM userspace daemon to dedicated PCPU
+print('vMigrater: Migrate single I/O Thread to running vCPUs...')
 pid=os.getpid()
-os.sched_setaffinity(pid, {0})
-#vCPUs = get_available_vCPUs()
-#print(vCPUs)
+#set migrate program to dedicated VCPU
+os.sched_setaffinity(pid, {1})
 io_pid=int(sys.argv[1])
+exiting=0
 try:
     affinity = os.sched_getaffinity(io_pid)
 except ProcessLookupError:
     print("Task %d might be finished (or not I/O intensive)" % io_pid)
 vcpu = affinity.pop()
 _vcpu = int(vcpu)
-print("I/O intensive pid: %d, running on vCPU %d" % (io_pid, _vcpu))
-exiting=0
-flag=0
 while 1:
-    try:
-        sleep(3.0/1000.0)
-    except KeyboardInterrupt:
-        exiting = 1
-# timestamp is in microseconds.
-    #vCPUs = []
     with open(vCPU_Sorted_RTS, 'r') as infile:
         try:
             vCPUs = json.load(infile)
         except ValueError:
             pass
-        #try:
-        #    os.sched_setaffinity(pid, {vCPUs[0][1]})
-        #except IndexError:
-        #    pass
-    #if I/O intensive thread is running on dead vCPU,
-    #migrate to vCPU with maximum remaining time
-    #for i in vCPUs:
-    #    if int(i[1]) == _vcpu:
-    #        flag = 1
-    #        if int(i[0]) < int(4000):
-    #            try:
-    #                os.sched_setaffinity(io_pid, {vCPUs[0][1]})
-    #                _vcpu = int(vCPUs[0][1])
-    #            except ProcessLookupError:
-    #                print("Task %d might be finished (or not I/O intensive)" % io_pid)
-    #            #print("IN: I/O Thread is migrated to vCPU %d" % vCPUs[0][1])
-    #if flag == 0:
-    try:
-        os.sched_setaffinity(io_pid, {vCPUs[0][1]})
-        #_vcpu = int(vCPUs[0][1])
-    except IndexError:
-        pass
-        #print("OUT: I/O Thread is migrated to vCPU %d" % vCPUs[0][1])
-    #else:
-    #    flag = 0
-    #print("flag is %d" % flag)
+    if len(vCPUs) == 0:
+        continue
+    #print("I/O thread is on vcpu %d" % _vcpu)
+    #try:
+    #    affinity = os.sched_getaffinity(io_pid)
+    #except ProcessLookupError:
+    #    print("Task %d might be finished (or not I/O intensive)" % io_pid)
+    #vcpu = affinity.pop()
+    #_vcpu = int(vcpu)
+    #print(vCPUs)
+    if _vcpu != vCPUs[0][1]:
+        try:
+            os.sched_setaffinity(io_pid, {vCPUs[0][1]})
+            _vcpu=vCPUs[0][1]
+        except IndexError:
+            pass
+        except OSError:
+            pass
     if exiting:
         print("Exiting...")
         exit()
