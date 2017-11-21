563b2bd366eaf83558a3e00acc07cc54fe903031
Weiwei Jia <wj47@njit.edu>
2017-11-21 02:45:35 -0500

    revise main backstage program for new settings

--- vMigrater/motivation/vcpu.shared/main_backstage.c
+++ vMigrater/motivation/vcpu.shared/main_backstage.c
@@ -129,8 +129,8 @@ struct shared_mem {
 };
 
 //FIXME: fix hardcoded
-static int start_vcpu = 1;
-static int end_vcpu = 8;
+static int start_vcpu = 2;
+static int end_vcpu = 7;
 
 static struct vcpu *vcpu;
 static struct _vcpu *_vcpu;
